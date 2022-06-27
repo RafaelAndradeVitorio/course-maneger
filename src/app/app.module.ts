3d4b92149aa61639ac3f8a2706e1ae31a1c916c0
Rafael Andrade <raafael4212@gmail.com>
2022-06-27 19:32:35 -0300

    criando rotas e usando rotas padroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { StarComponent } from './star/star.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Componet } from './error-404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: "", redirectTo: "courses", pathMatch: "full"
+    },
+    {
+      path: "courses", component: CoursesComponent
+    },
+    {
+      path: '**', component: Error404Componet
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
